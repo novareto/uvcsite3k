6bd2c0f023597d541bf5b67b1bd89db52b83af58
Christian Klinger <ck@novareto.de>
2019-06-06 10:40:50 +0200

    Let the basic Login Stuff works, add a cookie creds plugin, add some login forms, use waitress instead of paster

--- src/uvcsite/browser/views.py
+++ src/uvcsite/browser/views.py
@@ -3,4 +3,12 @@ import uvcsite.browser
 import uvcsite.app
 import uvcsite.resource
 
+from uvcsite.interfaces import IUVCSite
 
+
+
+class Index(grok.View):
+    grok.context(IUVCSite)
+
+    def render(self):
+        return u"Hallo Welt"
